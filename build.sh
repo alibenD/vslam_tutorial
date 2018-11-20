e3038d5ed0f9fe04308d5f817ab5fde2e2f4d713
alibenD <aliben.develop@gmail.com>
2018-11-20 10:31:42 +0800

    Init

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#---Automatically Generated from template 'bash' wrote by @aliben---
+# @Copyright (C) 2018 All rights reserved.
+# @file: 
+# @author: aliben.develop@gmail.com
+# @created_date: 2018-11-20 10:31:08
+# @last_modified_date: NO_LAST_MODIFIED_DATE
+# @brief: TODO
+# @details: TODO
+#---***********************************************---
+
+
+#---Variables
+CREATED_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+CREATED_YEAR=`date '+%Y'`
+
+#---Shell Command
+set -x
+cd build
+cmake ..
+make -j7
---
+++ src/print_hello.cc
@@ -0,0 +1,19 @@
+/**-----------------------------------------------
+  * @Copyright (C) All rights reserved.
+  * @filename: visual_slam_tutorial
+  * @author: aliben.develop@gmail.com
+  * @created_date: 2018-11-20 10:31:08
+  * @last_modified_date: NO_LAST_MODIFIED_DATE
+  * @description: TODO
+  *-----------------------------------------------*/
+
+//INCLUDE
+#include <visual_slam_tutorial/print_hello.hh>
+
+//CODE
+
+int print_hello()
+{
+  std::cout << "Hello world. -- Aliben auto create." << std::endl;
+  return 0;
+}
