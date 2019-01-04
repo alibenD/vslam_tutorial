d08efbe348f38fc2c576d0faefa6f7a85155d786
alibenD <aliben.develop@gmail.com>
2019-01-04 18:15:16 +0800

    Updated visual_slam_tutorial -> visual_slam

--- examples/hello_world.cc
+++ examples/hello_world.cc
@@ -3,12 +3,12 @@
   * @filename: visual_slam_tutorial
   * @author: aliben.develop@gmail.com
   * @created_date: 2018-11-20 10:31:08
-  * @last_modified_date: NO_LAST_MODIFIED_DATE
+  * @last_modified_date: 2019-01-04 18:14:06
   * @description: TODO
   *-----------------------------------------------*/
 
 //INCLUDE
-#include <visual_slam_tutorial/print_hello.hh>
+#include <visual_slam/print_hello.hh>
 
 //CODE
 
