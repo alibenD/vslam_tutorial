89bdcd24abb33aa049ea2b67b31114d42b79171a
alibenD <aliben.develop@gmail.com>
2019-01-15 10:27:42 +0800

    Updated test case for config yaml

--- test/test_config_yaml.cc
+++ test/test_config_yaml.cc
@@ -5,7 +5,7 @@
   * @version: v0.0.1
   * @author: aliben.develop@gmail.com
   * @create_date: 2019-01-14 14:15:18
-  * @last_modified_date: 2019-01-14 15:23:31
+  * @last_modified_date: 2019-01-14 21:41:16
   * @brief: TODO
   * @details: TODO
   */
@@ -25,7 +25,8 @@ class ConfigYAMLTest : public testing::Test
       config_yaml_ = std::make_shared<ak::ConfigYAML>();
       empty_file_path_ = "empty.yaml";
       std::ofstream outfile(empty_file_path_);
-      outfile << "%YAML:1.0\n";
+      outfile << "%YAML:1.0" << std::endl;
+      outfile << "testInt: 100" << std::endl;
       outfile.close();
     }
 
@@ -48,6 +49,12 @@ TEST_F(ConfigYAMLTest, release)
 {
   config_yaml_->open("empty.yaml");
   EXPECT_EQ(config_yaml_->release(), 0);
+}
+
+TEST_F(ConfigYAMLTest, read)
+{
+  config_yaml_->open("empty.yaml");
+  EXPECT_EQ(config_yaml_->getItem<int>("testInt"), 100);
 }
 
 int main(int argc, char** argv)
