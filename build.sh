ff5671a90122e1c66948a5ec40b5a17b18360faf
alibenD <aliben.develop@gmail.com>
2019-03-01 11:05:37 +0800

    Updated building system

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # @file: build.sh
 # @author: aliben.develop@gmail.com
 # @created_date: 2018-11-20 10:31:08
-# @last_modified_date: 2019-02-17 16:49:08
+# @last_modified_date: 2019-02-24 18:01:11
 # @brief: TODO
 # @details: TODO
 #---***********************************************---
@@ -25,7 +25,13 @@ if [ "${CLEAN}" == "clean" ]; then
   rm -rf build/*
   rm -rf lib/*
 fi
+
+if [ "${CLEAN}" == "debug" ]; then
+  BUILD_TYPE=DEBUG
+else
+  BUILD_TYPE=RELEASE
+fi
 ln -s `pwd`/log ./bin/log
 cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 make -j7
